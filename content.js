12a1bfa49ee5827462fa259557392a9cc94a1ebf
stellab95 <stellab95@hotmail.fr>
2025-01-10 10:52:12 +0100

    code fonctionnel

--- content.js
+++ content.js
@@ -1,41 +1,24 @@
-const btnValider = document.getElementById("btnValider")
-const input = document.getElementById("task").innerText
-console.log("input", input)
-const p = document.getElementById("displayTask")
+const input = document.querySelector("input")
+const button = document.querySelector("button")
+const ul = document.createElement("ul")
+const card = document.getElementById("card")
 
-const ulList = document.createElement("ul")
+card.appendChild(ul);
 
-let listTache = []
+let taskList = []
 
-btnValider.addEventListener('click', function() {
-    console.log(document.querySelector('#task').value)
+button.addEventListener('click', () => {
+   let userTask = input.value
+    taskList.push(userTask)
+    ul.innerHTML = ""; // on vide la liste avant de réafficher les tâches
+    input.value = ""
 
-    let task= document.querySelector('#task').value
+    taskList.forEach((task) => {
+        const taskElement = document.createElement("li")
+        taskElement.innerHTML = task // la tâche courante
 
-    listTache.push(task)// pour ajouter les tâches dans le tableau grace a la méthode push et à la variable taskAdd  qui stoke [] le tableau
-    console.log(listTache)
-    const listElement = document.createElement("li")
-  listTache.forEach((task) =>{ // boucle forEach qui va servir a mettre un liste dedan ( utilise la id task)
-    // const listElement = document.createElement("li")
-    console.log(listElement);
-    
-    listElement.innerHTML = task//pour chaque tâche il crée une liste
-    document.body.appendChild(listElement)
-    //taskList.appendChild(input)
-  })
-})
-
-// function inputValue(){
-// const input = document.getElementById('task').value // pour recupérer l'input dans le html
-// btnValider.addEventListener('click', () =>{
-//     console.log(input)
-// document.getElementById("displayTask").innerHTML = input  // on récupère et on affiche dans la page html la valeur de l'input
-// taskAdd.forEach(task => {
-//     console.log(task)
-
-// })
-// //console.log(taskAdd);
-// })
-// return input
-// }
-// console.log(inputValue())
+        ul.style.listStyle = "none";
+        ul.appendChild(taskElement)
+   })  
+   console.log(taskList);
+});
