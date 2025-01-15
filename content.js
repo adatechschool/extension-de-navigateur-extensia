01cd47eec0be6023bf6a1f754b8bdd2b6b9ac964
stellab95 <stellab95@hotmail.fr>
2025-01-15 15:37:10 +0100

    maj de tous les fichiers

--- content.js
+++ content.js
@@ -1,14 +1,10 @@
 const card = document.getElementById("card")
 const input = document.querySelector("input")
-const button = document.querySelector("button")
+const button = document.getElementById("btnValider")
 const containerList = document.getElementById("containerList")
 const ul = document.createElement("ul")
-const date = document.getElementById("date")
-<<<<<<< HEAD
 const trashButtonWhite = document.querySelector('.trashButton')
-
-=======
->>>>>>> a73f5de (add content.js)
+const clearContainer = document.getElementById("clearContainer") // ajout container bouton clear
 
 button.innerHTML = "Valider"
 containerList.appendChild(ul);
@@ -23,7 +19,8 @@ let uppercaseDate =  new Date().toLocaleDateString('fr-FR',{
 uppercaseDate = uppercaseDate.charAt(0).toUpperCase() + uppercaseDate.slice(1);
 date.innerHTML = uppercaseDate;
 
-let taskList = []
+let taskList = JSON.parse(localStorage.getItem('taskList')) || [] //
+console.log("taskList", taskList)
 
 input.addEventListener('keypress', function(event){
    if(event.key === "Enter"){
@@ -32,22 +29,33 @@ input.addEventListener('keypress', function(event){
  })
 
 button.addEventListener('click', () => {
-   let userTask = input.value
+   let userTask = input.value.trim() 
 
    if (!userTask) {
       alert("Veuillez saisir une tâche 😉 !");
+      input.value = ""
       return;
   }
 
     taskList.push(userTask)
     // ul.innerHTML = ""; // on vide la liste avant de réafficher les tâches
     input.value = "";
+
+    localStorage.setItem("taskList", JSON.stringify(taskList))
+    console.log(JSON.stringify(taskList));
+ 
     const listElement = document.createElement("li")
     const label = document.createElement("label")
     const trashButton = document.createElement("button")
     const inputCheckbox = document.createElement("input")
 
+    trashButton.addEventListener('click', function(){
+      listElement.remove()
+     })
+
     taskList.forEach((task) => {
+      listElement.classList.add('animate__animated', 'animate__slideInDown')
+      savedTheme=localStorage.getItem('theme')
         trashButton.classList.add("trashButton")
         inputCheckbox.classList.add("taskUnChecked")
         inputCheckbox.type = "checkbox"
@@ -56,14 +64,26 @@ button.addEventListener('click', () => {
          // on vérifie si les tâches sont cochées ou non
          if(inputCheckbox.checked){
             label.style.textDecorationLine = "line-through"               
-            label.style.color = "#8f8f9d"              
-         } else {
-            label.style.color = "black"              
-            label.style.textDecorationLine = "none"               
-         }
-         console.log(inputCheckbox.checked);
+            label.style.color = "#8f8f9d" 
+            console.log("le theme est (input checked)", savedTheme)
 
-      });      
+         } else if (!inputCheckbox.checked && localStorage.getItem('theme')=== 'dark'){
+         label.style.color = "#ffffff"
+         label.style.textDecorationLine = "none"     
+           console.log("le theme est", savedTheme)
+         }else{
+            label.style.textDecorationLine = "none"
+             label.style.color = "#8f8f9d" 
+            console.log("le theme est", savedTheme)
+         }
+      });
+
+            // if( savedTheme=== 'dark'){
+            //    label.style.color = "#8f8f9d"  
+            // }
+             /*label.style.color = "#8f8f9d"*/
+            //savedtheme=== 'dark'? label.style.color = "black"  :    label.style.color = "black"         
+            /*label.style.textDecorationLine = "none"*/               
 
         label.innerText = task // affiche la tâche courante
 
@@ -75,5 +95,40 @@ button.addEventListener('click', () => {
    })  
    console.log(taskList);
 });
+
+// créer un bouton qui vide le localStorage
+const clearButton = document.createElement('button')
+clearButton.classList = 'clearButton'
+clearContainer.appendChild(clearButton)
+
+clearButton.addEventListener('click', () => {
+   localStorage.clear()
+   location.reload()
+})
+
+const toggleButton = document.getElementById("dark-mode-toggle")
+const body= document.body
+
+toggleButton.addEventListener('click',() => {
+   
+body.classList.toggle('dark-mode');
+
+if(body.classList.contains('dark-mode')){
+   localStorage.setItem('theme','dark')
+}else{
+   localStorage.setItem('theme','light');
+}
+});
+
+let savedTheme= ''
+chrome.addEventListener('load',()=>{
+savedTheme=localStorage.getItem('theme');
+if(savedTheme=== 'dark'){
+   body.classList.add('dark-mode');
+}
+});
+
+
+
 
 
